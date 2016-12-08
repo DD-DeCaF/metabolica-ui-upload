39e9b1928a0dfd31ba36f3616efca94ceb67a9e0
Henning Redestig <henning.red@gmail.com>
2016-12-08 10:09:52 +0100

    fix: revert debug code..

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-// export const API_ROOT_URL = 'http://api.dd-decaf.eu';
-export const API_ROOT_URL = 'http://localhost:8000';
+export const API_ROOT_URL = 'http://api.dd-decaf.eu';
+// export const API_ROOT_URL = 'http://localhost:8000';
